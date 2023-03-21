b913e86622b571e04b6d94db2ab61a0f2af1a515
Andrew Sinemoglou <63246095+asinemog@users.noreply.github.com>
2023-03-21 22:25:25 +1100

    Week 6 Project created

---
+++ Week6Project/Week6Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Week6Project.c
+ *
+ * Created: 21/03/2023 10:24:20 PM
+ * Author : asine
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
