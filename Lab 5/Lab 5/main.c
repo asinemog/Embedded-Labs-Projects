48a6f564e4e9c73800f660e7533dc5bb810daddf
Andrew Sinemoglou <63246095+asinemog@users.noreply.github.com>
2023-03-23 21:03:13 +1100

    Added first slide of lab to test

--- Lab 5/Lab 5/main.c
+++ Lab 5/Lab 5/main.c
@@ -6,16 +6,31 @@
  */ 
 
 #include <avr/io.h>
-#include "util/delay.h"
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#define BAUD 9600
+#define MY_UBBR F_CPU/BAUD/16 - 1
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+    UBRR0 = MY_UBBR;
+	
+	UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
+	
+	UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01);
+    
+	unsigned char tmp = 0;
+	while (1) 
     { 
-		test;
-    }
+		while(!(UCSR0A & (1<<RXC0)));
+		tmp = UDR0;
+		while(!(UCSR0A & (1<<UDRE0)));
+		UDR0 = tmp + 1;
+	}
 }
 
 
