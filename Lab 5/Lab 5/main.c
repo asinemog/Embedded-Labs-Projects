a06d5c58f2596cd3fdffdc6650d026c0c761c218
Andrew Sinemoglou <63246095+asinemog@users.noreply.github.com>
2023-03-23 16:48:06 +1100

    Added USART lab 5 project

---
+++ Lab 5/Lab 5/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab 5.c
+ *
+ * Created: 23/03/2023 3:45:54 PM
+ * Author : asine
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
